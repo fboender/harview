334628d1b91d24b77325bc9a09c54fc3bd1407d2
Ferry Boender <ferry.boender@gmail.com>
2013-09-16 21:41:25 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,106 @@
+HARview
+=======
+
+About
+-----
+
+HARview is a HAR (HTTP Archive) viewer. HAR is a JSON-encoded recording of HTTP
+requests, responses and timings. .har Files can be exported from Chrome's
+Developer Tools' "Network" tab by capturing some traffic, right-clicking in the
+results pane and selecting 'Save as HAR with content'.
+
+HARView is a commandline tool which takes as input a .har (HTTP Archive) file
+and dumps a human-readable summary of it to the console. It is mostly useful
+for long dumps with many requests (such as debugging SAML / OAuth / etc). These
+can be captured in Chrome by enabling the "Preserve Log upon Navigation" button
+in the Network analyser.
+
+
+Requirements
+------------
+
+Python.
+
+
+Usage
+-----
+
+From the `--help`:
+
+    usage: harview.py [-h] [-v] [--filter-img] [--filter-refs] [--filter-all]
+                      [--nocolor]
+                      har_file
+    
+    positional arguments:
+      har_file
+    
+    optional arguments:
+      -h, --help     show this help message and exit
+      -v, --verbose  Show more. Can be used multiple times
+      --filter-img   Filter out requests for images
+      --filter-refs  Filter out requests for CSS, JS, etc
+      --filter-all   All filters
+      --nocolor
+
+Example:
+
+    harview.py -vv --filter-all img ./login.myorg.com.har
+
+The output of which would look something like:
+
+    301 GET http://electricmonk.nl/
+        Request headers
+        Response headers (status = 301)
+
+    200 GET http://www.electricmonk.nl/
+        Request headers
+            DNT: 1
+            Accept-Encoding: gzip,deflate,sdch
+            Host: www.electricmonk.nl
+            Accept-Language: en-US,en;q=0.8,nl-NL;q=0.6,nl;q=0.4
+            User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (
+                        KHTML, like Gecko) Ubuntu Chromium/28.0.1500.71 Chro
+                        me/28.0.1500.71 Safari/537.36
+            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/
+                    *;q=0.8
+            Cookie: PHPSESSID=210cd55ffbe6a18e104495ecee1e4356
+            Connection: keep-alive
+        Response headers (status = 200)
+            Date: Mon, 16 Sep 2013 18:02:05 GMT
+            Transfer-Encoding: chunked
+            Server: Apache
+            Connection: Keep-Alive
+            Keep-Alive: timeout=15, max=100
+            X-Pingback: http://www.electricmonk.nl/log/xmlrpc.php
+            Content-Type: text/html; charset=UTF-8
+
+    200 GET http://fonts.googleapis.com/css?family=Jura
+        Request headers
+            DNT: 1
+            Accept-Encoding: gzip,deflate,sdch
+            Host: fonts.googleapis.com
+            Accept-Language: en-US,en;q=0.8,nl-NL;q=0.6,nl;q=0.4
+            User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (
+                        KHTML, like Gecko) Ubuntu Chromium/28.0.1500.71 Chro
+                        me/28.0.1500.71 Safari/537.36
+            Accept: text/css,*/*;q=0.1
+            Referer: http://www.electricmonk.nl/
+            Connection: keep-alive
+        Response headers (status = 200)
+            Date: Mon, 16 Sep 2013 18:02:05 GMT
+            X-Content-Type-Options: nosniff
+            Server: GSE
+            X-Frame-Options: SAMEORIGIN
+            Content-Type: text/css
+            Alternate-Protocol: 80:quic
+            Cache-Control: private, max-age=86400
+            Timing-Allow-Origin: *
+            Content-Length: 239
+            X-XSS-Protection: 1; mode=block
+            Expires: Mon, 16 Sep 2013 18:02:05 GMT
+
+
+License
+-------
+
+MIT.
---
+++ src/harview.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+
+import sys
+import json
+import argparse
+import urlparse
+import os
+
+def get_terminal_size():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+    return int(cr[1]), int(cr[0])
+
+def color(color):
+    if color:
+        sys.stdout.write('\033[%sm' % (30 + color));
+    else:
+        sys.stdout.write('\033[0m')
+
+def text_wrap(s, cols=72, ident=4, ident_first=True):
+    """
+    Hard-wrap a string on chars, not words. Returns a string with the newlines
+    places in the correct positions.
+    """
+    new_s = ''
+    if ident_first:
+        new_s += ' ' * ident
+    pos = 0
+    for i in range(len(s)):
+        c = s[i]
+        if pos == cols - ident:
+            # Text is about to run off the right side.
+            new_s += '\n' + ' ' * ident + c
+            pos = 0
+        elif c == '\n':
+            # Naturally occuring newline in text.
+            new_s += '\n'
+            if i != len(s) - 1:
+                new_s += ' ' * ident
+            pos = 0
+        else:
+            new_s += c
+        pos += 1
+    return(new_s)
+
+if __name__ == "__main__":
+    global options
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='count', default=0, help="Show more. Can be used multiple times")
+    parser.add_argument('--filter-img', action='store_true', help="Filter out requests for images")
+    parser.add_argument('--filter-refs', action='store_true', help="Filter out requests for CSS, JS, etc")
+    parser.add_argument('--filter-all', action='store_true', help="All filters")
+    parser.add_argument('--nocolor', action='store_true', default=False)
+    parser.add_argument('har_file')
+    args = parser.parse_args()
+
+    url_filter = set()
+    if args.filter_refs or args.filter_all:
+        url_filter.update(('.js', '.css', '.woff'))
+    if args.filter_img or args.filter_all:
+        url_filter.update(('.png', '.jpg', '.jpeg', '.gif'))
+
+    cols, lines = get_terminal_size()
+
+    try:
+        har = json.load(file(args.har_file, 'r'))
+    except IndexError:
+        sys.stderr.write("Usage: %s <file.har>\n" % (sys.argv[0]))
+        sys.exit(1)
+    except ValueError, e:
+        sys.stderr.write("Invalid .har file: %s\n" % (str(e)))
+        sys.exit(2)
+
+    for entry in har['log']['entries']:
+        # Filter out any requests we don't want to see
+        url = urlparse.urlparse(entry['request']['url'])
+        fname, ext = os.path.splitext(url.path)
+        if ext in url_filter:
+            continue
+
+        if not args.nocolor:
+            i = str(entry['response']['status'])[0]
+            if i in ['4', '5']: # Client / Server error
+                color(1)
+            elif i in ['2']: # OK reponses
+                color(2)
+            else: # Other responses
+                color(3)
+
+        sys.stdout.write(str(entry['response']['status']))
+
+        if not args.nocolor:
+            color(None)
+
+        sys.stdout.write(' %s %s\n' % (
+                entry['request']['method'],
+                entry['request']['url']
+            )
+        )
+
+        if args.verbose > 0:
+            # Print headers
+            sys.stdout.write('    Request headers\n')
+            for header in entry['request']['headers']:
+                ident = len('        %s: ' % (header['name']))
+                sys.stdout.write('        %s: %s\n' % (
+                        header['name'],
+                        text_wrap(header['value'], cols=cols, ident=ident, ident_first=False)
+                    )
+                )
+
+            if args.verbose > 1:
+                # Print request body
+                if entry['request']['method'] == 'POST':
+                    sys.stdout.write('    POST data (%s)\n' % (entry['request']['postData']['mimeType']))
+                    sys.stdout.write(text_wrap(entry['request']['postData']['text'] + '\n',  cols=cols, ident=8))
+
+            sys.stdout.write('    Response headers (status = %s)\n' % (entry['response']['status']))
+            for header in entry['response']['headers']:
+                sys.stdout.write('        %s: %s\n' % (
+                    header['name'], header['value']
+                    )
+                )
+
+            if args.verbose > 1:
+                # Print response body
+                if 'text' in entry['response']['content']:
+                    sys.stdout.write('    Response data (%s)\n' % (entry['response']['content']['mimeType']))
+                    sys.stdout.write(text_wrap(entry['response']['content']['text'] + '\n',  cols=cols, ident=8))
+
+
+            sys.stdout.write('\n')
