cba65682f3b047a3324ce67e3ef0bddab7ca69fd
Michael Aquilina <michaelaquilina@gmail.com>
2018-01-09 21:28:04 +0000

    Add setup.py for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+try:
+    from setuptools import setup, find_packages  # noqa
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='harview',
+    version='0.2.0',
+    description=(
+        'A commandline tool which takes as input a .har (HTTP Archive) file '
+        'and dumps a human-readable summary of it to the console'
+    ),
+    long_description='',
+    license='GPLv3',
+    author='Ferry Boender',
+    url='https://github.com/fboender/harview',
+    scripts=[
+        'src/harview',
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
